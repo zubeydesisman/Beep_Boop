47c22829bbb5021e185b9e1d5b56b0dc98da68fb
Student <example@example.com>
2019-08-23 12:50:16 -0700

    add for loop to catch number 3 in user input

--- js/script.js
+++ js/script.js
@@ -2,12 +2,19 @@ $(document).ready(function() {
   $("form#numbers").submit(function(event) {
     event.preventDefault();
     var numbers = parseInt($("input#number").val());
-    var firstSpec = [...Array(numbers).keys()];
-   console.log(firstSpec);
+    var range = [...Array(numbers).keys()];
+     //console.log(range);
+    var three = [3];
+    for (var i=0; i<three.length; i++){
+      for (var j=0; j<range.length; j++) {
+      if (three[i] === range[j]) {
+      range = "I'm sorry, Dave. I'm afraid I can't do that.";
+}
+        }
+    }
 
 
-
-
+ console.log(range);
 
 
 
