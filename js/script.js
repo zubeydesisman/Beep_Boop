b59fe49f51c77904ec1097d9c7e6405c4335f276
Student <example@example.com>
2019-08-23 16:41:56 -0700

    Rewriting script with using split and push method and make a range with forloop.

--- js/script.js
+++ js/script.js
@@ -1,49 +1,59 @@
 $(document).ready(function() {
   $("form#numbers").submit(function(event) {
     event.preventDefault();
-    var numbers = parseInt($("input#number").val());
-    var array = [...Array(numbers).keys()];
-    var range = array + "," + numbers
-
-    console.log(range);
-
-
-
-
-
-
-//    var three = [3];
-//  for (var i=0; i<three.length; i++){
-//      for (var j=0; j<range.length; j++) {
-//        if (three[i] === range[j]) {
-//          range = "I'm sorry, Dave. I'm afraid I can't do that.";
-//      }
-//      }
-//    }
-//    var two = [2];
-//    for (var t=0; t<two.length; t++) {
-//      for (var j=0; j<range.length; j++) {
-//        if (two[t] === range[j]) {
-//          range = "Boop!";
-//        }
-//      }
-//    }
-//    var one = [1];
-//    for (var k=0; k<two.length; k++) {
-//      for (var j=0; j<range.length; j++) {
-//        if (one[k] === range[j]) {
-//          range = "Beep!";
-//        }
-//      }
-//    }
+    var numbers = $("input#number").val();
+  //  console.log(numbers);
+    var splitNumbers = numbers.split();
+          console.log(splitNumbers);
+     var range = [];
+     for (var i = 0; i<=numbers; i++)
+    var newRange = range.push(i);
+     console.log(range);
+//}
 
 
 
 
+    //
+    // var condition = [1,2,3];
+    // for (var i=0; i<condition.length; i++){
+    //   for (var j=0; j<lastRange.length; j++) {
+    //     if (condition[2] === lastRange[j]) {
+    //       lastRange[j] = "I'm sorry, Dave. I'm afraid I can't do that.";
+    //     if ( condition[1] === lastRange[j]){
+    //        lastRange[j] =  "Boop!"
+    //     if ( condition[0] === lastRange[j]) {
+    //        lastRange = "Beep!";
+    //     }
+    //     }
+    //       console.log(lastRange);
+    //     }
+    //   }
+    // }
+    //    var two = [2];
+    //    for (var t=0; t<two.length; t++) {
+    //      for (var j=0; j<range.length; j++) {
+    //        if (two[t] === range[j]) {
+    //          range = "Boop!";
+    //        }
+    //      }
+    //    }
+    //    var one = [1];
+    //    for (var k=0; k<two.length; k++) {
+    //      for (var j=0; j<range.length; j++) {
+    //        if (one[k] === range[j]) {
+    //          range = "Beep!";
+    //        }
+    //      }
+    //    }
 
 
 
-    console.log(range);
+
+
+
+
+
 
 
 
