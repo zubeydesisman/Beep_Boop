7f67ead9e8c7700d407563622651b22ac981afaa
Zubeyde Sisman <zubeydesisman@gmail.com>
2019-08-23 23:31:31 -0700

    Finish the if statementsand add a class to index.html to show result

--- js/script.js
+++ js/script.js
@@ -1,78 +1,25 @@
 $(document).ready(function() {
   $("form#numbers").submit(function(event) {
     event.preventDefault();
-    var numbers = $("input#number").val();
-  //  console.log(numbers);
-    var splitNumbers = numbers.split();
-      //    console.log(splitNumbers);
-     var thirt = ["I'm sorry, Dave. I'm afraid I can't do that."]
-     var range = [];
-     for (var i = 0; i<=numbers; i++)
-     range.push(i);
-// console.log(range);
-     var conditionsNumber = [1,2,3];
-     if (range[i] === (conditionsNumber[2])) {
-       range[i] = thirt
-       console.log(range);
-     }
-    //var thirt = ["I'm sorry, Dave. I'm afraid I can't do that."]
+    var userInput = $("input#number").val();
 
+    var output = [];
+    for (var sayi = 0; sayi<=userInput; sayi++) {
+      if (sayi.toString().includes("3")) {
+        output.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      } else if (sayi.toString().includes("2")) {
+        output.push("Boop!");
+      } else if (sayi.toString().includes("1")) {
+        output.push("Beep!");
+      } else {
+        output.push(sayi);
+      }
 
-
-    //
-    // var condition = [1,2,3];
-    // for (var i=0; i<condition.length; i++){
-    //   for (var j=0; j<lastRange.length; j++) {
-    //     if (condition[2] === lastRange[j]) {
-    //       lastRange[j] = "I'm sorry, Dave. I'm afraid I can't do that.";
-    //     if ( condition[1] === lastRange[j]){
-    //        lastRange[j] =  "Boop!"
-    //     if ( condition[0] === lastRange[j]) {
-    //        lastRange = "Beep!";
-    //     }
-    //     }
-    //       console.log(lastRange);
-    //     }
-    //   }
-    // }
-    //    var two = [2];
-    //    for (var t=0; t<two.length; t++) {
-    //      for (var j=0; j<range.length; j++) {
-    //        if (two[t] === range[j]) {
-    //          range = "Boop!";
-    //        }
-    //      }
-    //    }
-    //    var one = [1];
-    //    for (var k=0; k<two.length; k++) {
-    //      for (var j=0; j<range.length; j++) {
-    //        if (one[k] === range[j]) {
-    //          range = "Beep!";
-    //        }
-    //      }
-    //    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+var lastOutput = output.join(', ');
+    $(".lastOutput").text(lastOutput);
+      $("#result").show();
+    console.log(output);
 
   });
 });
